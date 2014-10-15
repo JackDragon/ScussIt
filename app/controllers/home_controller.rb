856e4e75c8c1376187bc3376e9467fa7b094a34f
jonwu <jonwu92@gmail.com>
2014-10-15 03:50:03 -0700

    add get request to thetvdb api and convert xml data to json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,21 @@
 class HomeController < ApplicationController
+
+	def index
+		
+	end
+
+	def getseries
+		require 'net/http'
+		require 'json'
+
+		url = URI.parse('http://thetvdb.com/data/series/'+params['id']+'/')
+		req = Net::HTTP::Get.new(url.to_s)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+		  http.request(req)
+		}
+		xml = res.body
+		json = Hash.from_xml(xml).to_json
+		render :json => json
+	end
+	
 end
