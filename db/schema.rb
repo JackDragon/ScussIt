c3d33e32a132d237889adb2705a6afe76f29b785
jonwu <jonwu92@gmail.com>
2014-10-19 15:23:48 -0700

    add schema from jonathan's computer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141012232455) do
+ActiveRecord::Schema.define(version: 20141017042900) do
 
   create_table "channels", force: true do |t|
     t.string   "name"
