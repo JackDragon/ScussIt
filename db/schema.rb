4440138a596749feb2cf0585051faafe71f5deb2
Jack Long <r.jack.long@gmail.com>
2014-10-19 19:15:31 -0700

    DB update for channel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019235632) do
+ActiveRecord::Schema.define(version: 20141020014550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20141019235632) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
+    t.string   "network"
+    t.string   "api_id"
   end
 
   create_table "messages", force: true do |t|
