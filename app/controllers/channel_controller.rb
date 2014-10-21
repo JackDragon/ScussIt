33916ea91ad512d08ffa40ce9212371af6b0a382
Jack Long <r.jack.long@gmail.com>
2014-10-21 01:41:03 -0700

    Temp routing, view, controller stuff for follow

--- app/controllers/channel_controller.rb
+++ app/controllers/channel_controller.rb
@@ -9,4 +9,15 @@ class ChannelController < ApplicationController
 
   def browse
   end
+
+  def room
+    if params.has_key?(:id)
+      id = params(:id).to_i
+      @room = Room.find(id)
+      render index
+    end
+  end
+
+  def follow
+  end
 end
