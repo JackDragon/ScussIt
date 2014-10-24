09f59f69edb1fb3ba3a3f74110bdd6ccbe204050
Jonathan Lo <jon.lo@berkeley.edu>
2014-10-23 21:39:35 -0700

    Added some documentation, modified .git_ignore list

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id            :integer          not null, primary key
+#  body          :string(255)
+#  creation_time :datetime
+#  created_at    :datetime
+#  updated_at    :datetime
+#  user_id       :integer
+#  channel_id    :integer
+#
+
 require 'rails_helper'
 
 RSpec.describe Message, :type => :model do
