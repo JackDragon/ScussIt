fb0eb9819fcebca783c7dba62dd34d33cb970e70
Jonathan Lo <jon.lo@berkeley.edu>
2014-10-23 20:09:06 -0700

    some cursory spec edits

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,14 @@ describe User do
     # before :each do
     # end
 
-    it "should not fail user save" do
-      u = User.new(email: "jackrlong@yahoo.com")
+    it "is valid User" do
+      u = User.new(email: "jon.lo@berkeley.edu", password: "123")
       expect(u.save).to be_valid
     end
+
+    it "password cannot be empty?" do
+      u = User.new(email: "jackrlong@yahoo.com")
+      expect(u.save).not_to be_valid
+    end
+
 end
