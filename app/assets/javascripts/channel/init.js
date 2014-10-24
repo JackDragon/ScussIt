0b305ff191f955580cd67040d746d2f9017b22e5
Jack Long <r.jack.long@gmail.com>
2014-10-24 15:03:18 -0700

    Huh temp

---
+++ app/assets/javascripts/channel/init.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	$(".browse_frame").click(function(event) {
+		
+	});
+
+	$(".sidebar .follow").click(function(event) {
+		
+	});
+});
+
---
+++ spec/helpers/application_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe ApplicationHelper, "#http_request" do
+	
+ 
+end
