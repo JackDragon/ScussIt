9e663a6c4dcfd1c8f4d6b47c3dd5968facb0a8f7
Jonathan Lo <jon.lo@berkeley.edu>
2014-10-24 21:03:51 -0700

    sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -4,22 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Heroku ActionMailer config
 # ActionMailer::Base.smtp_settings = {
-#   :address        => 'smtp.sendgrid.net',
-#   :port           => '587',
-#   :authentication => :plain,
-#   :user_name      => ENV['Kaz'],
-#   :password       => ENV['8Qyg%Z7gm#Dg'],
-#   :domain         => 'heroku.com',
-#   :enable_starttls_auto => true
-# 
-ActionMailer::Base.smtp_settings = {
-	:address => "smtp.gmail.com",
-	:port => 587,
-	:domain => 'heroku.com',  
-	:authentication => :plain, 
-	:enable_starttls_auto => true,
-	:user_name => 'jon.lo.hs@gmail.com',
-	:password => 'n1blheim'
-}
+# 	:address => "smtp.gmail.com",
+# 	:port => 587,
+# 	:domain => 'heroku.com',  
+# 	:authentication => :plain, 
+# 	:enable_starttls_auto => true,
+# 	:user_name => 'jon.lo.hs@gmail.com',
+# 	:password => 'n1blheim'
+# }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,19 +59,26 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
 
-  config.action_mailer.default_url_options = {:host => 'myapp.herokuapp.com', :protocol => 'http'}
+  config.action_mailer.default_url_options = {:host => 'http://warm-mountain-1838.herokuapp.com/', :protocol => 'http'}
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-     :address => "smtp.gmail.com",
-     :port => 587,
-     :domain => 'myapp.herokuapp.com',  #I've also tried changing this to 'gmail.com'
-     :authentication => :plain, # I've also tried changing this to :login
-     :enable_starttls_auto => true,
-     :user_name => 'jon.lo.hs@gmail.com',
-     :password => 'n1blheim'
-   }
+   #   :address => "smtp.gmail.com",
+   #   :port => 587,
+   #   :domain => 'myapp.herokuapp.com',  #I've also tried changing this to 'gmail.com'
+   #   :authentication => :plain, # I've also tried changing this to :login
+   #   :enable_starttls_auto => true,
+   #   :user_name => ENV['jon.lo.hs@gmail.com'],
+   #   :password => ENV['n1blheim']
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
