6658739f64db7ca990eedf8c90ad8d789ee8438a
Jonathan Lo <jon.lo@berkeley.edu>
2014-10-24 20:28:17 -0700

    mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,20 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = {:host => 'myapp.herokuapp.com', :protocol => 'http'}
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :domain => 'myapp.herokuapp.com',  #I've also tried changing this to 'gmail.com'
+     :authentication => :plain, # I've also tried changing this to :login
+     :enable_starttls_auto => true,
+     :user_name => 'jon.lo.hs@gmail.com',
+     :password => 'n1blheim'
+   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
