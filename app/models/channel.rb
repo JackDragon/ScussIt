be7f6ace3410cf17665fd737bdfd1a0fcd691ad2
Jack Long <r.jack.long@gmail.com>
2014-11-22 17:47:56 -0800

    reverted active stuff

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -50,7 +50,7 @@ class Channel < ActiveRecord::Base
   end
 
   def self.get_topics(id)
-    channel = Channel.find(params[:id])
+    channel = Channel.find(id)
     topics = channel.topics
     return topics
   end
@@ -145,82 +145,62 @@ class Channel < ActiveRecord::Base
     return following
   end
 
-  def self.active_delete(params, current_user)
+   def self.active_delete(params, current_user)
     cid = nil
-    # topic_names = params[:topic_names]
     if params.has_key?(:cid)
       cid = params[:cid]
-      # topic_names = params[:topic_names]
     elsif params.has_key?(:api_id) and Channel.find_by(api_id: params[:api_id]) != nil
       cid = (Channel.find_by api_id: params[:api_id]).id
     end
     if current_user.actives.exists?(:channel_id => cid)
-      for to_delete in Active.where(:channel_id => cid, :user_id => current_user.id)[0]
-        Active.destroy(to_delete.id)
-      end
+      to_delete = Active.where(:channel_id => cid, :user_id => current_user.id)[0]
+      Active.destroy(to_delete.id)
     end
   end
 
   def self.active_update(params, current_user)
     if current_user != nil
       cid = nil
-      topic_names = params[:topic_names]
       if params.has_key?(:cid)
         cid = params[:cid]
       end
       if cid != nil
-        for top in topic_names
-          entry = Active.find_by(channel_id: cid, user_id: current_user.id, :topic_name => top)
-          if entry != nil
-            entry.update(updated: DateTime.now)
-          end
-        end
+        entry = Active.find_by(channel_id: cid, user_id: current_user.id)
+        entry.update(updated: DateTime.now)
       end
     end
   end
 
   def self.active_add(params, current_user)
     cid = nil
-    topic_names = nil
-    if params.has_key?(:topic_names)
-      topic_names = params[:topic_names]
-    else
-      topic_names = ["Main"]
-    end
     if params.has_key?(:cid)
       cid = params[:cid]
     elsif params.has_key?(:api_id) and Channel.find_by(api_id: params[:api_id]) != nil
       cid = (Channel.find_by api_id: params[:api_id]).id
     end
-    for top in topic_names
-      if cid != nil and !current_user.actives.exists?(:channel_id => cid, :topic_name => top)
-        current_user.actives.create(:channel_id => cid, :topic_name => top, :updated => DateTime.now)
-      end
+      
+    if cid != nil and !current_user.actives.exists?(:channel_id => cid)
+      current_user.actives.create(:channel_id => cid, :updated => DateTime.now)
     end
   end
 
   def self.active_user_list(params, current_user)
+    l = []
     # p params
-    active_dict = {}
-    topics = Topic.where(:channel_id => cid)
-    for top in topics
-      l = []
-      timenow = DateTime.now
-      cid = params[:id]
-      if cid != nil and Active.exists?(:channel_id => cid, :topic_name => top.topic_name)
-        l = Active.where(:channel_id => cid, :topic_name => top.topic_name).where("updated > ?", timenow-5.seconds)
-      end
-      userlist = []
-      for entry in l
-        # p "ENTRY"*100
-        # p entry
-        eid = entry.user_id
-        username = User.where(:id => eid)[0].username
-        # entry["username"] = username
-        userlist += [username]
-      end
-      active_dict[top.topic_name] = userlist
-      return active_dict
+    timenow = DateTime.now
+    cid = params[:id]
+    if cid != nil and Active.exists?(:channel_id => cid)
+      l = Active.where(:channel_id => cid).where("updated > ?", timenow-5.seconds)
     end
+    userlist = []
+    for entry in l
+      # p "ENTRY"*100
+      # p entry
+      eid = entry.user_id
+      username = User.where(:id => eid)[0].username
+      # entry["username"] = username
+      userlist += [username]
+    end
+    return userlist
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121064850) do
+ActiveRecord::Schema.define(version: 20141123014316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20141121064850) do
     t.integer  "user_id"
     t.integer  "channel_id"
     t.datetime "updated"
-    t.string   "topic_name"
   end
 
   create_table "channels", force: true do |t|
