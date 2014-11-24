05337489cbbf967a7f047dc592613862aa40a470
Gangbaolede Li <itepsilons@berkeley.edu>
2014-11-23 23:45:02 -0800

    bug fixed

--- selenium_tests.rb
+++ selenium_tests.rb
@@ -50,72 +50,111 @@ send_message.send_keys :return
 
 # check message exists
 sleep(5)
-message = driver.find_element(:xpath, "//p[last()]").text
+message = wait.until {driver.find_element(:xpath, "//p[last()]")}
+message = message.text
 puts(message)
 if !message.include?("Test Sentence, Please Ignore.")
 	abort("Message not found")
 end
 
+sleep(5)
+
 # check emoticon
 send_message.send_keys ":)"
 send_message.send_keys :return
+
 sleep(5)
-message = driver.find_element(:xpath, "//p[last()]").text
-driver.find_element(:xpath, "//img[@src='/assets/smile.png']")
+
+message = wait.until {driver.find_element(:xpath, "//p[last()]")}
+message = message.text
+
+wait.until {driver.find_element(:xpath, "//img[@src='/assets/smile.png']")}
 
 # add topic
 send_message.send_keys "Hello #World"
 send_message.send_keys :return
 
-# click topic button
-topic_button = driver.find_element(:xpath, "//button[@class='btn btn-primary topics']")
-topic_button.click
 sleep(5)
+
+# click topic button
+topic_button = wait.until {driver.find_element(:xpath, "//button[@class='btn btn-primary topics']")}
+topic_button.click
+
+sleep(5)
+
 # # check newly added topic
 # newly_added_topic = wait.until {driver.find_element(:xpath, "//button[@data-bb-handler='World']")}
 newly_added_topic = wait.until {driver.find_element(:xpath, "//button[text()='#World']")}
 newly_added_topic.click
 
+sleep(5)
+
 # Check follow button
-follow_button = driver.find_element(:xpath, "//button[@class='btn btn-primary follow']")
+follow_button = wait.until {driver.find_element(:xpath, "//button[@class='btn btn-primary follow']")}
 follow_text = follow_button.text
 follow_button.click
 # assert {follow_text != follow_button.text}
+
 sleep(5)
 
 if (follow_text == follow_button.text)
 	abort("Follow button check failed")
 end
 
+sleep(5)
+
 # check favorites page
-driver.find_element(:xpath, "//a[@href='/mychannels']").click
+favorite = wait.until {driver.find_element(:xpath, "//a[@href='/mychannels']")}
+favorite.click
+
+sleep(5)
 
 # browse
 browse_page = wait.until {driver.find_element(:xpath, "//a[@href='/browse']")}
 browse_page.click
 
+sleep(5)
+
 # browse next
 browse_next = wait.until {driver.find_element(:xpath, "//input[@value='Next']")}
 browse_next.click
 
+sleep(5)
+
 # browse prev.
 browse_prev = wait.until {driver.find_element(:xpath, "//input[@value='Previous']")}
 browse_prev.click
+
+sleep(5)
 
 # search
 search = wait.until {driver.find_element(:xpath, "//input[@id='query']")}
 search.send_keys "blue"
 search.send_keys :return
 
+sleep(5)
+
 # Edit profile
 dropdown = wait.until {driver.find_element(:xpath, "//a[@class='dropdown-toggle']")}
 dropdown.click
-driver.find_element(:xpath, "//a[@href='/users/edit']").click
+
+sleep(5)
+
+edit = wait.until{driver.find_element(:xpath, "//a[@href='/users/edit']")}
+edit.click
+
+sleep(5)
 
 # Logout
 dropdown = wait.until {driver.find_element(:xpath, "//a[@class='dropdown-toggle']")}
 dropdown.click
-driver.find_element(:xpath, "//a[@href='/users/sign_out']").click
+
+sleep(5)
+
+signout = wait.until {driver.find_element(:xpath, "//a[@href='/users/sign_out']")}
+signout.click
+
+sleep(5)
 
 puts "All tests passed!"
 
